b3ffa3929b740356fb532ae117a3fd8b8f85987a
Bhuvan S <88939855+bhuvan2533@users.noreply.github.com>
2021-10-09 17:59:42 +0530

    Create 01.c

---
+++ Day-2/01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+    int eligibility;
+    printf("Enter your age :");
+    scanf("%d",&eligibility);
+    (eligibility>18)?printf("You are eligible to vote !!"):printf("You are not eligible to vote");
+}
