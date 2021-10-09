cb76e8f642d6494b5fb0b65e92ebe6fdad58712b
Bhuvan S <88939855+bhuvan2533@users.noreply.github.com>
2021-10-09 17:59:29 +0530

    Delete 01.c

--- 01.c
+++
@@ -1,8 +0,0 @@
-
-#include <stdio.h>
-void main(){
-    int eligibility;
-    printf("Enter your age :");
-    scanf("%d",&eligibility);
-    (eligibility>18)?printf("You are eligible to vote !!"):printf("You are not eligible to vote");
-}
